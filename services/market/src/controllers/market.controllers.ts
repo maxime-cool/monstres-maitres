120b14d93b85c00e2f00f2375ea92d9d2a10c5c1
Camille <camille.pinton@imt-atlantique.net>
2024-01-07 22:36:06 +0100

    Market

--- services/market/src/controllers/market.controllers.ts
+++ services/market/src/controllers/market.controllers.ts
@@ -21,11 +21,16 @@ export const buyMonster = async (request: FastifyRequest, reply: FastifyReply) =
         master_id: master_id,
         team_id : team_id
     };
-    const monstreServerUrl = `http://monstre:5004/api/monstres/monstre`;
-    const response = await axios.post(monstreServerUrl, monstreData);
-    const creditUser : number = await axios.get(`http://users:5003/api/users/credits/${master_id}`);
-    const price = creditUser - dexResult.base_price;
-    const serverUrl = `http://users:5003/api/users/credits/${master_id}`
-    await axios.put(serverUrl, { credits : price});
+    const creditUser = await axios.get(`http://users:5003/api/users/credits/${master_id}`);
+    const price = creditUser.data[0].credits - dexResult.base_price;
+    if (price >=0) {
+        const serverUrl = `http://users:5003/api/users/credits/${master_id}`
+        await axios.put(serverUrl, { credits : price});
+        const monstreServerUrl = `http://monstre:5004/api/monstres/monstre`;
+        await axios.post(monstreServerUrl, monstreData);
+    }
+    else {
+        console.error('Error : not enough credits to buy monster');
+    }
 }
 
